626867613c1bd46997d7f849d07ff5517add137c
joannez8 <joannez8@nycstudents.net>
2025-10-03 15:04:19 -0400

    Edited test cases.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println(a.findFreeBlock(2,9));
         System.out.println(a.findFreeBlock(2,20));
         /** findFreeBlock test cases end */
+        schedule = new boolean[8][60];
         for(int i = 25; i < 30; i++) schedule[1][i] = true;
         for(int i = 0; i < 15; i++) schedule[2][i] = true;
         for(int i = 41; i < 60; i++) schedule[2][i] = true;
